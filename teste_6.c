913b861cf98ffaaa1590313816a6a8714f47380a
vbc1005 <vinicius.bernardes@sempreceub.com>
2025-03-12 13:53:30 -0300

    teste_6

--- teste_6.c
+++ teste_6.c
@@ -11,15 +11,16 @@ int main(int argc, char *argv[])
     
     printf ("Qual e o peso da primeira nota?: \n");
     scanf ("%f", &PESO_A);
-
+    
     printf ("Insira a segunda nota do aluno: \n");
     scanf ("%f", &B);
-
+    
     printf ("Qual e o peso da segunda nota?: \n");
     scanf ("%f", &PESO_B);
 
-    C = PESO_A = PESO_B;
-    MEDIA = ((A * PESO_A) + (B * PESO_B)/C);
+    C = PESO_A + PESO_B;
+    MEDIA = (A * PESO_A + B * PESO_B)/C;
+    
     printf("A media do aluno, eh: %.5f\n", MEDIA);
 return 0;
 }
